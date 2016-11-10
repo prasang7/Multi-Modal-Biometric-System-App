362c88e0ff88e5896fb5ed341883c6def55314d3
prasang7 <prasang.sharma7@gmail.com>
2016-11-10 23:39:41 +0530

    key generated

--- source-code/app/src/main/java/com/multimodalbiometricapp/Fingerprint_Scan.java
+++ source-code/app/src/main/java/com/multimodalbiometricapp/Fingerprint_Scan.java
@@ -6,10 +6,17 @@ import android.content.pm.PackageManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.security.keystore.KeyProperties;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
+import java.security.KeyStore;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
+import javax.crypto.KeyGenerator;
 
 /**
  * Created by prasang7 on 10/11/16.
@@ -19,6 +26,8 @@ public class Fingerprint_Scan extends AppCompatActivity {
 
     private FingerprintManager fingerprintManager;
     private KeyguardManager keyguardManager;
+    private KeyStore keyStore;
+    private KeyGenerator keyGenerator;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,7 +49,6 @@ public class Fingerprint_Scan extends AppCompatActivity {
                 return;
             }
         }
-
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.USE_FINGERPRINT) !=
                 PackageManager.PERMISSION_GRANTED) {
@@ -50,7 +58,6 @@ public class Fingerprint_Scan extends AppCompatActivity {
 
             return;
         }
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (!fingerprintManager.hasEnrolledFingerprints()) {
 
@@ -63,7 +70,32 @@ public class Fingerprint_Scan extends AppCompatActivity {
         }
 
 
+
+
     }
+
+
+    protected void generateKey() {
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            keyGenerator = KeyGenerator.getInstance(
+                    KeyProperties.KEY_ALGORITHM_AES,
+                    "AndroidKeyStore");
+        } catch (NoSuchAlgorithmException |
+                NoSuchProviderException e) {
+            throw new RuntimeException(
+                    "Failed to get KeyGenerator instance", e);
+        }
+
+
+    }
+
+
 
 
 }

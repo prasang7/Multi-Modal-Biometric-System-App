499e49ae9b52cea04ab487143e44e20cae61093a
prasang7 <prasang.sharma7@gmail.com>
2016-11-10 23:30:20 +0530

    Keygaurd and Fingerprint manager initiated

--- source-code/app/src/main/java/com/multimodalbiometricapp/Fingerprint_Scan.java
+++ source-code/app/src/main/java/com/multimodalbiometricapp/Fingerprint_Scan.java
@@ -1,10 +1,15 @@
 package com.multimodalbiometricapp;
 
+import android.Manifest;
 import android.app.KeyguardManager;
+import android.content.pm.PackageManager;
 import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 /**
  * Created by prasang7 on 10/11/16.
@@ -24,6 +29,39 @@ public class Fingerprint_Scan extends AppCompatActivity {
                 (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
         fingerprintManager =
                 (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            if (!keyguardManager.isKeyguardSecure()) {
+
+                Toast.makeText(this,
+                        "Lock screen security not enabled in Settings",
+                        Toast.LENGTH_LONG).show();
+                return;
+            }
+        }
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.USE_FINGERPRINT) !=
+                PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this,
+                    "Fingerprint authentication permission not enabled",
+                    Toast.LENGTH_LONG).show();
+
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!fingerprintManager.hasEnrolledFingerprints()) {
+
+                // This happens when no fingerprints are registered.
+                Toast.makeText(this,
+                        "Register at least one fingerprint in Settings",
+                        Toast.LENGTH_LONG).show();
+                return;
+            }
+        }
+
 
     }
 

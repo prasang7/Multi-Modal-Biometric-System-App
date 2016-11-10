62370aa4444431c0427607ec6dd09da9c4ae91e3
prasang7 <prasang.sharma7@gmail.com>
2016-11-11 00:17:14 +0530

    Authentication and Callback methods added

--- source-code/app/src/main/java/com/multimodalbiometricapp/FingerprintHandler.java
+++ source-code/app/src/main/java/com/multimodalbiometricapp/FingerprintHandler.java
@@ -1,10 +1,14 @@
 package com.multimodalbiometricapp;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Build;
 import android.os.CancellationSignal;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
 
 /**
  * Created by prasang7 on 11/11/16.
@@ -20,5 +24,48 @@ public class FingerprintHandler extends
         appContext = context;
     }
 
-    
+    public void startAuth(FingerprintManager manager,
+                          FingerprintManager.CryptoObject cryptoObject) {
+
+        cancellationSignal = new CancellationSignal();
+
+        if (ActivityCompat.checkSelfPermission(appContext,
+                Manifest.permission.USE_FINGERPRINT) !=
+                PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        manager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
+    }
+
+    @Override
+    public void onAuthenticationError(int errMsgId,
+                                      CharSequence errString) {
+        Toast.makeText(appContext,
+                "Authentication error\n" + errString,
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onAuthenticationHelp(int helpMsgId,
+                                     CharSequence helpString) {
+        Toast.makeText(appContext,
+                "Authentication help\n" + helpString,
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onAuthenticationFailed() {
+        Toast.makeText(appContext,
+                "Authentication failed.",
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onAuthenticationSucceeded(
+            FingerprintManager.AuthenticationResult result) {
+
+        Toast.makeText(appContext,
+                "Authentication succeeded.",
+                Toast.LENGTH_LONG).show();
+    }
 }
